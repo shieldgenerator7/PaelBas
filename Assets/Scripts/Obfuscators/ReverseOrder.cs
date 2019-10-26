7211f678c5d4d24159dafb0eefc21b5813a0976d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-10-26 09:01:09 -0400

    Added ReverseOrder obfuscator

---
+++ Assets/Scripts/Obfuscators/ReverseOrder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseOrder : Obfuscator
+{
+    public override string obfuscate(string text)
+    {
+        string output = "";
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            output += text[i];
+        }
+        return output;
+    }
+    public override string unobfuscate(string untext)
+    {
+        //it's cool that reversing a reversed string unobfuscates it
+        return obfuscate(untext);
+    }
+}
