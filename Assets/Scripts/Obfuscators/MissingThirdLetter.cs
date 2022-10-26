4126cb9c980a591a60c58bc82563aadd7ecfe3e5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-26 11:29:46 -0400

    Allow MissingThirdLetter to be missing a letter in any position

--- Assets/Scripts/Obfuscators/MissingThirdLetter.cs
+++ Assets/Scripts/Obfuscators/MissingThirdLetter.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="MissingFirstLetter", menuName = "Obfuscator/MissingFirstLetter")]
+[CreateAssetMenu(fileName = "MissingLetter", menuName = "Obfuscator/MissingLetter")]
 public class MissingThirdLetter : Obfuscator
 {
+    [Range(1, 10)]
+    [Tooltip("The position of the letter to remove. Base 1")]
+    public int missingLetterPosition = 3;
+    [Tooltip("The character to use to fill in for the missing character when unobfuscating")]
     public string missingFillerChar = "?";
 
     public override string obfuscate(string text)
@@ -21,13 +25,13 @@ public class MissingThirdLetter : Obfuscator
             for (int iW = 0; iW < words.Length; iW++)
             {
                 string word = words[iW];
-                if (word == "" || word.Length < 3)
+                if (word == "" || word.Length < missingLetterPosition)
                 {
                     continue;
                 }
-                string head = "" + word[0] + word[1];
-                string tail = (word.Length > 3)
-                    ? word.Substring(3)
+                string head = word.Substring(0, missingLetterPosition - 1);
+                string tail = (word.Length > missingLetterPosition)
+                    ? word.Substring(missingLetterPosition)
                     : "";
                 words[iW] = head + tail;
             }
@@ -37,6 +41,7 @@ public class MissingThirdLetter : Obfuscator
     }
     public override string unobfuscate(string untext)
     {
+        int prevLetterPosition = missingLetterPosition - 1;
         string[] segments = getSegments(untext);
         for (int iS = 0; iS < segments.Length; iS++)
         {
@@ -49,14 +54,14 @@ public class MissingThirdLetter : Obfuscator
             for (int iW = 0; iW < words.Length; iW++)
             {
                 string word = words[iW];
-                if (word == "" || word.Length < 2)
+                if (word == "" || word.Length < prevLetterPosition)
                 {
                     continue;
                 }
-                string head = "" + word[0] + word[1];
+                string head = word.Substring(0, missingLetterPosition - 1);
                 string middle = missingFillerChar;
-                string tail = (word.Length > 2)
-                    ? word.Substring(2)
+                string tail = (word.Length > prevLetterPosition)
+                    ? word.Substring(prevLetterPosition)
                     : "";
                 words[iW] = head + middle + tail;
             }
