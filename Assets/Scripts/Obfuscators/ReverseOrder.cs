71de528a92f363a611c98c3e1b99efb52cb87cfd
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-10-26 12:44:45 -0400

    ReverseOrder reverses each word individually

--- Assets/Scripts/Obfuscators/ReverseOrder.cs
+++ Assets/Scripts/Obfuscators/ReverseOrder.cs
@@ -7,12 +7,32 @@ public class ReverseOrder : Obfuscator
 {
     public override string obfuscate(string text)
     {
-        string output = "";
-        for (int i = text.Length - 1; i >= 0; i--)
+        string[] segments = getSegments(text);
+        for (int iS = 0; iS < segments.Length; iS++)
         {
-            output += text[i];
+            string segment = segments[iS];
+            if (string.IsNullOrWhiteSpace(segment))
+            {
+                continue;
+            }
+            string[] words = getWords(segment);
+            for (int iW = 0; iW < words.Length; iW++)
+            {
+                string word = words[iW];
+                if (string.IsNullOrWhiteSpace(word))
+                {
+                    continue;
+                }
+                string output = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    output += word[i];
+                }
+                words[iW] = output;
+            }
+            segments[iS] = setWords(words, segment);
         }
-        return output;
+        return setSegments(segments, text);
     }
     public override string unobfuscate(string untext)
     {
