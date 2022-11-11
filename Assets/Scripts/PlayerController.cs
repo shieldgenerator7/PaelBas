047a480387c9250877424be77767f7616bf6fec8
shieldgenerator7 <shieldgenerator7@yahoo.com>
2022-11-11 05:37:35 -0500

    Clicking unknown character highlights it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     public TMP_InputField txtMessage;
 
+    private TMP_Text lblMessage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,28 @@ public class PlayerController : MonoBehaviour
         MessagePuzzleManager.instance.onObfuscatorPushed += (obf) => updateText();
         MessagePuzzleManager.instance.onObfuscatorPopped += updateText;
         txtMessage.ActivateInputField();
+        lblMessage = txtMessage.GetComponentInChildren<TMP_Text>();
         updateText();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //2022-11-11: copied from https://stackoverflow.com/a/57691490/2336212
+            var charIndex = TMP_TextUtilities.FindIntersectingCharacter(lblMessage, Input.mousePosition, Camera.main, false);
+
+            //Debug.Log($"charIndex: {charIndex}");
+            if (charIndex != -1)
+            {
+                char clickedChar = lblMessage.textInfo.characterInfo[charIndex].character;
+                if (clickedChar == '?' || clickedChar == '_')
+                {
+                    txtMessage.selectionAnchorPosition = charIndex;
+                    txtMessage.selectionFocusPosition = charIndex + 1;
+                }
+            }
+        }
     }
 
     public void adjustMessageIndex(int addend)
